31d2f996886aadd380dddbd574af8a4d8cd25b19
metacogpe <metacog@kakao.com>
2020-06-21 19:02:51 +0900

    Ver0.9
    기본 기능 확보

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+import random
 from pymongo import MongoClient
 
 from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
-client = MongoClient('mongodb://test:test@localhost', 27017)
+#client = MongoClient('mongodb://test:test@localhost', 27017)
+client = MongoClient('localhost', 27017)
+
 db = client.dbterms
 
 
@@ -20,28 +23,11 @@ def terms_list():
     # 2. 성공하면 success 메시지와 함께 terms_list 목록을 클라이언트에 전달합니다.
 
     terms_list = list(db.learnterm.find({},{'_id':False}) ) # 커서를 list로 변환 
+    random.shuffle(terms_list)
 
     print(terms_list)
     
     return jsonify({'result': 'success','msg':'list 연결되었습니다!', 'terms_list':terms_list})
-
-
-@app.route('/api/search', methods=['POST'])
-def term_like():
-    # 1. 클라이언트가 전달한 term_give를 term_receive 변수에 넣습니다.
-    term_receive = request.form['term_give']
-    print(term_receive)
-    # 2. learnterm 목록에서 find_all으로 each_term이 term_receive와 일치하는 each_term을 찾습니다.
-    search_term = db.learnterm.find_all({'each_term':term_receive})
-    print(search_term)
-    # 3. term의 like 에 1을 더해준 new_like 변수를 만듭니다.
-    new_like = term['like'] + 1
-    print(new_like)
-    # 4. mystar 목록에서 name이 name_receive인 문서의 like 를 new_like로 변경합니다.
-    db.learnterm.update_one({'name':name_receive},{'$set': {'like':new_like}})
-
-    # 5. 성공하면 success 메시지를 반환합니다.
-    return jsonify({'result': 'success','msg':'like 연결되었습니다!'})
 
 
 @app.route('/api/like', methods=['POST'])
@@ -51,14 +37,62 @@ def term_like():
     print(term_receive)
     # 2. learnterm 목록에서 find_all으로 each_term이 term_receive와 일치하는 each_term을 찾습니다.
     term = db.learnterm.find_one({'each_term':term_receive})
+    
     # 3. term의 like 에 1을 더해준 new_like 변수를 만듭니다.
     new_like = term['like'] + 1
     # print(new_like)
     # 4. mystar 목록에서 name이 name_receive인 문서의 like 를 new_like로 변경합니다.
     db.learnterm.update_one({'each_term':term_receive},{'$set': {'like':new_like}})
     # 5. 성공하면 success 메시지를 반환합니다.
-    return jsonify({'result': 'success','msg':'like 연결되었습니다!'})
+    return jsonify({'result': 'success','msg':'복습완료! 수고하셨습니다!'})
+
+@app.route('/api/unlike', methods=['POST'])
+def term_unlike():
+    # 1. 클라이언트가 전달한 name_give를 name_receive 변수에 넣습니다.
+    term_receive = request.form['term_give']  # index.html에서 제공하는 'term_give' 로 정보처리 
+    # 2. mystar 목록에서 find_one으로 name이 name_receive와 일치하는 star를 찾습니다.
+    term = db.learnterm.find_one({'each_term':term_receive})
+     # 3. star의 like 에 1을 더해준 new_like 변수를 만듭니다.
+    new_like = term['like'] - 1
+    if new_like <0:
+        return jsonify({'result':'success', 'msg':'0 이하가 될 수 없습니다.'})
+    else:
+    # 4. mystar 목록에서 name이 name_receive인 문서의 like 를 new_like로 변경합니다.
+        db.learnterm.update_one({'each_term':term_receive},{'$set': {'like':new_like}})
+        return jsonify({'result': 'success','msg':'다시 보겠습니다!'})
+
+
+
+@app.route('/api/delete', methods=['POST'])
+def term_delete():
+    # 1. 클라이언트가 전달한 name_give를 name_receive 변수에 넣습니다.
+    term_receive = request.form['term_give']
+    # 2. mystar 목록에서 delete_one으로 name이 name_receive와 일치하는 star를 제거합니다.
+    db.learnterm.delete_one({'each_term':term_receive})
+    # 3. 성공하면 success 메시지를 반환합니다.
+    return jsonify({'result': 'success','msg':'delete 되었습니다!'})
+
+
+# @app.route('/api/search', methods=['POST'])
+# def term_like():
+#     # 1. 클라이언트가 전달한 term_give를 term_receive 변수에 넣습니다.
+#     term_receive = request.form['term_give']
+#     print(term_receive)
+#     # 2. learnterm 목록에서 find_all으로 each_term이 term_receive와 일치하는 each_term을 찾습니다.
+#     search_term = db.learnterm.find_all({'each_term':term_receive})
+#     print(search_term)
+#     # 3. term의 like 에 1을 더해준 new_like 변수를 만듭니다.
+#     new_like = term['like'] + 1
+#     print(new_like)
+#     # 4. mystar 목록에서 name이 name_receive인 문서의 like 를 new_like로 변경합니다.
+#     db.learnterm.update_one({'name':name_receive},{'$set': {'like':new_like}})
+
+#     # 5. 성공하면 success 메시지를 반환합니다.
+#     return jsonify({'result': 'success','msg':'like 연결되었습니다!'})
+
+
+
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=80, debug=True)
--- init_db.py
+++ init_db.py
@@ -2,18 +2,34 @@ import requests
 from bs4 import BeautifulSoup
 
 from pymongo import MongoClient
+import random
 
-client = MongoClient('mongodb://test:test@localhost', 27017)
+client = MongoClient('localhost', 27017)
+#client = MongoClient('mongodb://test:test@localhost', 27017)
 db = client.dbterms
 
-# 용어별 링크에 접근하기 위한 Seq.번호를 가져오기 
-def get_word_seq():
+# 최종 페이지 번호 가져오기 
+def last_page():
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.post('https://terms.tta.or.kr/dictionary/dictionaryNewWordList.do', headers=headers, data={'listPage':1})
+    data = requests.post('https://terms.tta.or.kr/dictionary/dictionaryNewWordList.do', headers=headers, data={'listPage':1}) ## 첫 페이지에서 마지막 페이지 크롤링 
     soup = BeautifulSoup(data.text, 'html.parser')
 
-    each_page = soup.select('#m_content > ul > li > dl')
+    last_page = soup.select('#m_content > div > div > span.last >a')[0]['href']  # 최종 페이지 찾기 
+    last_page_no = last_page[-4:-2] # 최종 페이지 번호 
+    last_page_no = int(last_page_no)
+    print(type(last_page_no))
+    return last_page_no
+print(last_page())
 
+
+# 페이지별로 용어별 링크에 접근하기 위한 Seq.번호를 가져오기 
+def get_word_seq(last_page_no):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.post('https://terms.tta.or.kr/dictionary/dictionaryNewWordList.do', headers=headers, data={'listPage':last_page_no}) ## POST형식으로 웹 사이트 조회 
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    each_page = soup.select('#m_content > ul > li > dl')  
+    
     word_seqs =[]
     for one in each_page:
         word_seq = one.select('dl > dt > a')[0]['href']
@@ -32,32 +48,46 @@ def get_term(word_seq):
     soup = BeautifulSoup(data.text, 'html.parser')
     
     each_term = soup.select_one('#cont > dl > dt').text.strip() 
-    each_desc = soup.select_one('#cont > dl > dd > div').text
+    each_desc = soup.select_one('#cont > dl > dd > div').text    
     each_img = soup.select_one('#cont > dl > dd > div').find('img')
-    image_src = None
+    img_src = None
     if None != each_img:
-        image_src = each_img['src']
+        img_src = each_img['src']
 
 
     doc = {
     'each_term': each_term,
     'each_desc': each_desc,
-    #'each_img': each_img    
-    # 'term_like : term_like    
+    'each_img': img_src,    
+    'like' : 0    
     }
     # DB에 하나의 레코드 Insert
     db.learnterm.insert_one(doc)
     print('완료!\n', each_term)
-    print(image_src)
+    print(img_src)
 
 
 # 기존 learnterm 콜렉션을 삭제하고, 출처 ter 가져온 후, 크롤링하여 DB에 저장합니다.
 def insert_all():
-    db.learnterm.drop()  # learnterm 콜렉션을 모두 지워줍니다.
-    print(get_word_seq())
-    word_seqs =  get_word_seq()
-    for word_seq in word_seqs:
-        get_term(word_seq)
+    db.learnterm.drop()  # learnterm 콜렉션을 모두 지우기
+
+    last_page_no = last_page() # 마지막 페이지 번호를 가져오기 
+
+    word_seqs = []
+
+    for i in range(last_page_no): # 페이지 번호별로 용어 가져오기 위해서 페이지 번호별로 루프 동작 
+        # word_seqs.append(get_word_seq(i+1))
+        for word_seq in get_word_seq(i+1):  # 각 페이지에 존재하는 용어 시퀀스 번호 
+            get_term(word_seq)              # 각 페이지에서 시퀀스 번호별로 용어 가져오기 
+        
+
+    #word_seqs = get_word_seq() # 페이지별로 용어의 시퀀스 번호를 가져오기
+    # print(word_seqs)
+    #random.shuffle(word_seqs)
+    #print(word_seqs)
+    
+    # for word_seq in word_seqs:
+        # get_term(word_seq)
 
 
 ### 웹페이지에서 가져온 데이터를 모두 DB에 Insert하기 
